f371301b6b9e3ace5b54de1913274128efae3cb0
WihlC <willchartrand@gmail.com>
2022-04-26 15:26:23 -0400

    I don't know why this isn't working now

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const withPlugins = require('next-compose-plugins')
-const withImages = require('next-images')
-const withVideos = require('next-videos')
-
-module.exports = withPlugins([withImages, withVideos], {
-  target: 'serverless',
-})

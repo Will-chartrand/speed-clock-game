abab509f8d28c933a218e723f0a02de8db2f8204
WihlC <willchartrand@gmail.com>
2022-05-10 14:18:26 -0400

    changing setclock and sethand functions

--- pages/setClock.js
+++ pages/setClock.js
@@ -1,7 +1,4 @@
-const setClock = function(clockName) {
+export const setClock = function(clockName) {
       // Set the clock image to the specified clock image
     document.querySelector("#clockBody").setAttribute('src', clockName); // Set the clock name to string argument given
 }
-
-export default setClock;
-
